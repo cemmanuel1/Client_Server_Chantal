9bc0f5794e107e509d55827806aa9e35b590b9f6
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-05 07:49:57 -0700

    completed

--- public/js/application.js
+++ public/js/application.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
   $('.awesomeness_teller').on('click', function() {
-  	var setVal = $(this).attr('data-heading');
-  	$('#awesomeness_holder').html('<h2>'+setVal+'</h2>');
+    var setVal = $(this).attr('data-heading');
+    $('#awesomeness_holder').html('<h2>'+setVal+'</h2>');
   });
 
-$('.skill_teller').on('click', function(){
-	var setSkill = window.location.pathname.replace('/skills/', '');
-	$('#skill_holder').text(setSkill);
-});
 
-$('.meal_teller').on('click', function(){
-	$('#meal_holder').text(sampleMeal);
+    $('.skill_teller').on('click', function(){
+      var setSkill = window.location.pathname.replace('/skills/','');
+      $('#skill_holder').html('<h2>'+setSkill+'</h2>');
+    });
+    
+   $('.meal_teller').on('click', function(){
+      $('#meal_holder').html('<h2>'+sampleMeal+'</h2>');
+    });
+
+  // What other events do you need to bind to make the other pages work?
 });
